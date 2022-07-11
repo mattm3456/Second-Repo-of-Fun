c4b8be5023f3faca1dc87be513fa25cbf85d0b0f
mattm3456 <107494541+mattm3456@users.noreply.github.com>
2022-07-11 10:42:30 -0400

    Add files via upload

---
+++ PackageTest.py
@@ -0,0 +1,6 @@
+def add_1(x):
+    '''add_1 returns your input plus one.
+    Inputs: a number (integer or float)
+    Outputs: your inputted number, plus one
+    '''
+    return x + 1
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Matt and Dominicks Package of Fun',
+    version='1.0.0',
+    url='https://github.com/mattm3456/Dominick-and-Matts-Repo-of-Fun',
+    author='Matt and Dominick',
+    description='Test Function that adds 1',
+    packages=find_packages(),    
+    license = 'LICENSE.txt'
+)
